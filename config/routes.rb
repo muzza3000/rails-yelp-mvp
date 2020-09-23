c602ca79b2ec75a212435f7451dcad0fcf320b2b
Thomas Murray <thomas.murray09@gmail.com>
2020-09-23 22:29:13 +0200

    all working

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants, except: %i[update destroy] do
-    # collection do
-    # end
-    # member do
-    # end
     resources :reviews, only: %i[new create]
   end
-  # resources :reviews, except: %i[index new create]
 end
+
+
