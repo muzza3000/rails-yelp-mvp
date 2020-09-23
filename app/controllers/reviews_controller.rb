e32ed32706cba97873348c445d08a43ed98305b4
Thomas Murray <thomas.murray09@gmail.com>
2020-09-23 13:51:04 +0200

    finsihed with styling and functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,36 @@
 class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+    @review = Review.find(params['id'])
+  end
+
+  def new
+    # Only defined so we can reuse the form
+    @restaurant = Restaurant.find(params['restaurant_id'])
+    @review = Review.new
+  end
+
+  def create
+    # Find the restaurant instance from the
+    @restaurant = Restaurant.find(params['restaurant_id'])
+    # Create the review from the form contents
+    @review = Review.new(review_params)
+    # Make the link between the review and the restaurant
+    @review.restaurant = @restaurant
+
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ApplicationRecord
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :address, presence: true
